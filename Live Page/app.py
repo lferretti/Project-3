b9263cd4aa085c1f6abe95a354da540ddad99c8d
Alex Wolf <alexmattwolf@gmail.com>
2019-11-20 13:42:35 -0500

    Final Line Plot Attempt 2

--- Live Page/app.py
+++ Live Page/app.py
@@ -18,32 +18,19 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 df_19 = pd.read_csv('data/EURUSD-2019ForexData.csv')
-# i = 10
-
-# def iterate():
-#     i += 1
-#     return i
 
 def livefeed():
-#     live_df = df_19[:i]
-
-#     if (live_df.index.size > 60):
-#         live_df = live_df.iloc[-60]
-
     json_df = df_19.to_json(orient = 'records')
     return json_df
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("interactive.html")
 
 
 @app.route("/data")
 def data():
     return livefeed()
-    # i += 1
-
-# threading.Timer(60, data()).start()
     
 if __name__ == "__main__":
     app.run()
--- Live Page/static/assets/js/charts/app.js
+++ Live Page/static/assets/js/charts/app.js
@@ -1,33 +1,37 @@
-
+// Initialize the data
 getdata();
 
-
-var data, globaldata, reset;
-var date = [], year = [], month = [], day = [], hour = [], minute = [], weekday = [], actual = [], poschange = [], negchange = []
-// var gogo = false
+var data, globaldata;
+var minute = [], actual = [], poschange = [], negchange = []
+//Use gogo to turn interval off/on at need. Makes basic changes e.g. HTML easier to process
 var gogo = true
+// var gogo = false
 var max = 0
-
+//Stop e at max - 10 because it allows a 10 tick gap, allowing forecast trend lines to be more effective visually
 var e = Math.max(0, (max - 10));
 xrange = 40
 var s = 0;
 var lineID = 'plot';
 var barID = 'residual';
 
+//Make arrays at specified ranges, offset range2 for bar chart (bars now go between grid columns)
 var range = Array(xrange).fill().map((x, z) => z);
 var range2 = Array(xrange).fill().map((x, z) => z + .5);
 
+//Select Filter button, define filter inputs
 var filterbtn = d3.select("#filter");
 filterbtn.on("click", filterer)
 var filtered, date_input, hour_input;
 var hour_in, date_in;
 
+//Sections make quick scrolling and overview navigation easier
 // FILTER FUNTCIONS
 // FILTER FUNTCIONS
 // FILTER FUNTCIONS
 // FILTER FUNTCIONS
 // FILTER FUNTCIONS
 
+//When filtered, reset interval, reset speed & speed input box, then filter data by date
 function filterer() {
 
   clear();
@@ -46,18 +50,17 @@ function filterer() {
   var date_chk = new Date(filtered[0]['date'])
   var hour_chk = filtered[0]['hour']
 
+  //If date does not contain hour then go to select first associated with said date.
   if (String(date_chk) === String(date_in) && hour_chk != hour_in && hour_chk > hour_in) {
     d3.select('#hour').attr('value', hour)
     hour_in = hour_chk
   }
-  else if (String(date_chk) > String(date_in)) {
 
-  }
-  // console.log(date_chk, date_in, hour_chk, hour_in)
   OntheHour();
 }
 
-
+//'Filter' by hour, actually takes spliced data and find first instance of that hour and returns everything after it. FIlter by hour would only return data 
+// with that hour
 function OntheHour() {
 
   function ClockStrikes(y) {
@@ -73,6 +76,8 @@ function OntheHour() {
 }
 
 
+// Initalize data system. make sure globaldata is accessible globally. Note data is empty because the graph starts with no datapoint.
+// to make graph start with datapoints adjust initial iterator variables and slice data accordingly
 function getdata() {
 
   var url = `/data`;
@@ -82,7 +87,6 @@ function getdata() {
     data = []
     // xx.slice(s, e)
     globaldata = xx;
-    reset = xx;
 
   actual = [], minute = []
     
@@ -98,16 +102,29 @@ function getdata() {
 // Data Intitalizer
 // Data Intitalizer
 
+// When this function is called (filters), rest all the tables, plots, html text, iterators, etc...
 function makedata() {
 
   d3.select(`#${lineID}`).html('')
   d3.select(`#${barID}`).html('')
+  d3.select("#outbody").html('')
+  d3.select("#pip").style('color','black').text('Please Wait')
+  d3.select("#money").style('color','black').text('Please Wait')
+  d3.select("#action").style('color','black').style('font-weight','normal').text('Please Wait')
+  all_dict = ['Initial', '-', '-', '-', '-', '-', '10000']
+  tabelizer()
+  
 
   pts = [], avg = [], y0 = [], y1 = [], x0 = [], x1 = [], color = []
+  dinero = 10000
   signif = false
+  trend = 0
   wait = 0
+  avg = []
   actual = [], minute = [], poschange = [], negchange = [], shapes = []
   max = 0
+  pipchange = null, reco = null, 
+  
   e = Math.max(0, (max - 10));
   s = 0
 
@@ -130,6 +147,7 @@ function makedata() {
     clock()
 }
 
+// Get inital tick marks for live graph, make sure the minutes are in :00 format
 function clock() {
   clockdata = globaldata.slice(s, xrange)
 
@@ -144,7 +162,6 @@ function clock() {
       
     });
 
-    // console.log(minute)
     drawline()
 }
 
@@ -155,20 +172,27 @@ function clock() {
 // Data Call Iterators
 // Data Call Iterators
 
-var ydata, yneg, ypos, date, hour, minutex, runitback;
+var ydata, yneg, ypos, runitback;
 var shapeact = [], shapechange = [];
 runitback = false
 
+//Pass in new ytrace variables for line graph
 function yact() {
 
   e += 1;
   s += 1;
 
   ydata = globaldata[e]['actual']
+  shapeact.push(globaldata[e]['actual'])
+
+  if (shapeact.length > 30) {
+    shapeact.shift()
+  }
 
   return ydata;
 }
 
+//Pass in new bar graph traces variables, seperate them by positive or negative value so customer formating can occur (red or green bars)
 function ychange() {
 
   var delta = globaldata[e]['change']
@@ -191,7 +215,24 @@ function ychange() {
   return [ypos, yneg];
 }
 
+//Data to be exported to profit.js so date, hours, minutes can be included in reports. Note has to be e-11 due when the model recommended to Buy/Sell.
+//Not 10 points after
+function outdata() {
+  var exp = [];
+  var e11 = e - 11
+
+  exp.push(globaldata[e11]['date'])
+  exp.push(globaldata[e11]['hour'])
+  exp.push(globaldata[e11]['minute'])
+
+  return exp;
+}
+
+//New minutes and hours for graph axes to be updated ob
 function newdata() {
+
+  var date, hour
+
   date = globaldata[e]['date']
   hour = globaldata[e]['hour']
 
@@ -200,8 +241,10 @@ function newdata() {
 
 var e2 = xrange
 
+//Take the previously created minute array and update it to the new minute value pushed. Shift it by one to remove the old value
 function mini() {
 
+  //If e2 errors that means, the data has reached it's conclusion. at which point the models will reset and start again.
   try {
     e2= e2 + 1
     minim = globaldata[e2]['minute'] 
@@ -210,8 +253,7 @@ function mini() {
     console.log('Reset')
     clear()
     speedreset()
-    globaldata = reset
-    makedata()
+    getdata()
   }
 
   if (minim < 10) {
@@ -224,7 +266,6 @@ function mini() {
   minute.shift()
   // console.log(minute)
   return minute;
-
 }
 
 // GRAPHS
@@ -234,7 +275,7 @@ function mini() {
 // GRAPHS
 // GRAPHS
 
-
+//Draw the line data 'live' graph
 var linedata; 
 function drawline() {
 
@@ -259,7 +300,6 @@ function drawline() {
       },
       range: [0, 0],
       autorange: true,
-      // type: "linear",
       dtick: .0001,
       showticklabels: false,
     },
@@ -284,7 +324,7 @@ function drawline() {
 // BAR
 // BAR
 
-
+//Draw the bar graph
 var bardata; 
 function drawbar() {
 
@@ -356,12 +396,38 @@ var speed = 1000;
 var once = 0
 var shapes = []
 
+//Start the interval 'live' system
 function interval() {
+
+  //Due to bad initial y-axis labels. remove labels until interval starts
+  Plotly.relayout(lineID, {
+    yaxis: {
+      showticklabels: true,
+    }
+  })
+  Plotly.relayout(barID, {
+    yaxis: {
+      showgrid: true,
+      showticklabels: false,
+      title: `Change`,
+      orientation: 'h',
+      tickfont: {
+        size: 'auto',
+      },
+      range: [0, 0],
+      autorange: true,
+      type: "linear",
+      dtick: .0001,
+    },
+  })
+
+//Make sure interval can only be started once, prevents errors
 if (once == 0) {
   go = setInterval(() => {
 
     once = 1;
 
+    //Extend traces of line and bar grpah by attaching new data point to ends of graphs
     Plotly.extendTraces(lineID, {
       y: [[yact()]]}, [0]);
 
@@ -374,32 +440,22 @@ if (once == 0) {
     Plotly.extendTraces(barID, {
       y: [[changling[1]]]}, [1]);
 
+    //Shift the taces by removing first data point when e is greater than the extent
       xrng_adj = xrange-9
-
+    
+    //Call the shape making function before relayout, updates existing shapes and finds new ones
+      workshop()
+    
       if (e > xrng_adj) {
-        linedata[0].y.shift();
-        bardata[0].y.shift();
-        bardata[1].y.shift();
-      }
+      linedata[0].y.shift();
+      bardata[0].y.shift();
+      bardata[1].y.shift();
 
-      Plotly.relayout(lineID, {
-        yaxis: {
-          showticklabels: true,
-        }
-      })
-      Plotly.relayout(barID, {
-        yaxis: {
-          showticklabels: true,
-        },
-      })
-
-    workshop()
-
-    xrng_adj
-      if (e > xrng_adj) {
       labels = mini()
       
+      //Relayout both graphs, adding shapes to line graph and keeping the range of the x-axis static. Making the apperance of a moving graph
         Plotly.relayout(lineID, {
+          shapes: shapes,
           xaxis: {
             range: [0, xrange+1],
             title: `Hour: <b>${newdata()[1]}</b>, Day: ${newdata()[0]}`,
@@ -407,8 +463,7 @@ if (once == 0) {
             tickvals: range,
             tick0: 0,
             dtick: 1,
-          },
-          shapes: shapes,
+          }
         })
 
         Plotly.relayout(barID, {
@@ -434,15 +489,16 @@ if (once == 0) {
 //  BUTTONS
 //  BUTTONS
 
-
-
+//Button to speed up, slow down, pause, start the interval
 var speedset = d3.select("#speed")
 
+//outputs new speed to speed input box
 function spedometer() {
   speedout = document.getElementById('speed');
   speedout.value = speed/1000;
 }
 
+//if custom speed is entered updates, speed on interval
 speedset.on("change", function() {
   clear(); 
   speed = speedset.property("value")*1000;
@@ -456,7 +512,7 @@ speedset.on("change", function() {
   }
 })
 
-
+//stops interval so new speed can be entered, once = 0 so interval can be started again
 function clear() {
   once = 0;
   clearInterval(go)
@@ -466,6 +522,7 @@ function speedreset() {
   speed = 1000;
 }
 
+//Pauses the live graph
 var pausebtn = d3.select("#pause");
 pausebtn.on("click", function() {
   console.log('Pause')
@@ -476,6 +533,8 @@ pausebtn.on("click", function() {
 
 var speedout = d3.select("#speed")
 
+//resumes live graph movement at speed of 1 second. Logically if someone paused it they may not want to resume at full speed, rather look at it in detail
+//Thus reseting speed makes since
 var playbtn = d3.select("#play");
 playbtn.on("click", function() {
   clear()
@@ -484,6 +543,7 @@ playbtn.on("click", function() {
   interval()
 })
 
+//Speeds up, but maxes out a half a second, faster than that half a second and graph buttons start having hard time keeping up with graph data
 var fwdbtn = d3.select("#fwd");
 fwdbtn.on("click", function() {
   clear(); 
@@ -503,6 +563,7 @@ fwdbtn.on("click", function() {
 }
 )
 
+//opposite of fast button
 var slowbtn = d3.select("#slow");
 slowbtn.on("click", function() {
   clear(); 
@@ -528,31 +589,48 @@ slowbtn.on("click", function() {
 // SVG Trace ###
 // SVG Trace ###
 
-
-var pts = [], avg = [], y0 = [], y1 = [], x0 = [], x1 = [], color = [], profit, signif, trend, len, wait, ten
+var pts = [], avg = [], y0 = [], y1 = [], x0 = [], x1 = [], color = [], profit, signif, trend, len, wait, ten, pipchange, reco, output
 signif = false
 wait = 0
-ten = 10
+// ten = 10
 
+//initate shape making functionality
 function workshop() {
+
+  end = shapechange.length 
+  start = end - 15
+  pts = shapechange.slice(start, end)
+  actpts = shapeact.slice(start, end)
+
+  //if shape has been added recently wait 25 iterations before finding a new one. prevent over crowding and counteractive shape reports
   if (wait == 0 && e >= 15) {
     sum()
   }
   else if (wait > 0) {
     wait = Math.max(0, wait - 1)
-    ten = Math.min(10, ten + 1)
-    if (ten == 10) {
-      console.log('Profit Out', pts[end])
+    
+    //if at 10 point after the shape was determined (end of linear forecast) then output live data results to determine if profit or loss occured.
+    //exports data to profit.js
+    if (wait == 15) {
+
+      output = outdata()
+      pipchange =  Number(actpts[13] - actpts[3])
+
+      if (color[0] == 'green') {
+            reco = "Buy"
+      }
+      else {
+            reco = "Sell"
+      }
+
+      console.log(pipchange, actpts[13], actpts[3])
     }
   }
   craft()
 }
 
+//take the sum of the 15 most recent data points, then average them. Put average in array of 15 averages
 function sum() {
-  end = shapechange.length
-  start = end - 15
-  pts = shapechange.slice(start, end)
-  actpts = linedata[0].y.slice(start, end)
 
   // console.log('pts', pts)
   len = pts.length - 1
@@ -568,6 +646,8 @@ function sum() {
 
 }
 
+//If average of array of averages is positive or negative and greater than limit, then moving trend is found, significant is true.
+//Color shape accordingly, green and red and push x and y variables
 function avgcalc() {
 
   var endval, startval;
@@ -599,17 +679,18 @@ function avgcalc() {
     x1.push(end)
     y1.push(endval)
     wait = 25;
-    t = 10;
     console.log("Trend", trend, signif)
   }
   craft()
 
 }
 
+//Make the paper and line shapes
 function craft() {
   var adjx0 = [], adjx1 = [], adjy0 = [], adjy1 = []
   shapes = []
   // console.log(x1[0])
+  //If paper shape is now off page remove all variables associated with, deleting the shape
   if (x1[0] < 0) {
     x0.shift()
     y0.shift()
@@ -618,11 +699,12 @@ function craft() {
     color.shift()
   }
 
+  //make shapes with every trend that is found, due to wait timer on two can found one page at a time
   for(i = 0; i < x1.length; i++) {
 
   shapes.push({
-    type: 'rect',
     xref: 'x',
+    type: 'rect',
     yref: 'paper',
     x0: x0[i],
     y0: 0,
@@ -647,6 +729,7 @@ function craft() {
       y1: y1[i],
     })
 
+    //every foor loop iteration subtract one from the x and y variables to make shapes move accorss line graph
     adjx0.push(x0[i] - 1)
     adjy0.push(y0[i] - 1)
     adjx1.push(x1[i] - 1)
@@ -654,8 +737,5 @@ function craft() {
   }
 
   x0 = adjx0
-  // y0 = adjy0
   x1 = adjx1
 }
-
-
