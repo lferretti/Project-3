8b72542da5fb42ce15aea1e6145f0137d40b4609
Julian Freeman <jofreeman1014@gmail.com>
2019-11-21 17:45:16 -0500

    Update app.py
    Works now

--- src/heroku/flask/app.py
+++ src/heroku/flask/app.py
@@ -4,6 +4,8 @@ from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+df_19 = pd.read_csv('data/2018_12_forex_sel.csv')
  
 # CSV Imports
 # ecbi_df = pd.read_csv("data/ECB_Interest Rates.csv")
@@ -21,6 +23,13 @@ def interactive():
 @app.route("/summary")
 def summary():
     return render_template("Summary.html")
+
+
+@app.route("/data")
+def data():
+    json_df = df_19.to_json(orient = 'records')
+    return json_df
+
 
 # @app.route("/data") # Necessary for the javascript, needs a data path to pull data from the csv!
 # def dataset():
