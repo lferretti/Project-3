7e57a33868ced935676e3d4e9d7c3b792fb58fe0
jofreeman1014 <50783178+jofreeman1014@users.noreply.github.com>
2019-11-21 15:51:16 -0500

    heroku setup
    Heroku folder setup. None of the code is functional, but folder structure should be set up properly

---
+++ src/heroku/flask/app.py
@@ -0,0 +1,23 @@
+import os
+import pandas as pd
+from flask import Flask, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+ 
+# CSV Imports
+# ecbi_df = pd.read_csv("data/ECB_Interest Rates.csv")
+# frbi_df = pd.read_csv("data/FRB_Interest Rates.csv")
+
+# Base path
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+# For the Machine Learning path
+# @app.route("/ml")
+# def ml():
+#     return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run()
